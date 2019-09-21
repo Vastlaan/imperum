ebd56dc9ce9d1467aece33a15157f103f2b2200a
vastlaan <vastlaan@mail.com>
2019-09-21 17:40:31 +0200

    modern design step 2

--- client/src/Components/modernDesign.js
+++ client/src/Components/modernDesign.js
@@ -16,7 +16,18 @@ const ModernDesign = (style) =>{
 				<MdClose />
 			</div>
 			<div className='modern__main'>
+				<ul className='modern__main--menu'>
+					<li className='border-blue'>Contacts</li>
+					<li className='border-pink'>Contacts</li>
+					<li className='border-green'>Contacts</li>
+					<li className='border-red'>Contacts</li>
+				</ul>
 				<div className='modern__main--header'>
+					
+					<div className='modern__main--header-red'></div>
+					<div className='modern__main--header-blue'></div>
+					<div className='modern__main--header-yellow'></div>
+					<div className='modern__main--header-pink'></div>
 					<h1>Modern</h1>
 					<h1>Design</h1>
 				</div>
